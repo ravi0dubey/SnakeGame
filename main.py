202ec734dcbc161b89357793848f72c36bdc8ea5
ravi0dubey <ravi0dubey@gmail.com>
2022-05-05 21:55:02 -0400

    made changes

--- main.py
+++ main.py
@@ -10,11 +10,19 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
 game_continues = True
 while game_continues:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+
 
 
 
--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 STARTING_POSITIONS = [(-20,0), (0,0),(20,0)]
 MOVE_DISTANCE = 20
 MOVE_LEFT= 90
+MOVE_RIGHT = 90
 
 class Snake:
     def __init__(self):
@@ -22,4 +23,22 @@ class Snake:
             new_y = self.all_snake[seq_num - 1].ycor()
             self.all_snake[seq_num].goto(new_x, new_y)
         self.all_snake[0].forward(MOVE_DISTANCE)
-        self.all_snake[0].left(MOVE_LEFT)
+
+
+    def up(self):
+        self.all_snake[0].setheading(90)
+        self.self.all_snake[0].move()
+
+    def down(self):
+        self.all_snake[0].setheading(270)
+        self.self.all_snake[0].move()
+
+    def left(self):
+        self.all_snake[0].setheading(180)
+        self.self.all_snake[0].move()
+
+    def right(self):
+        self.all_snake[0].setheading(0)
+        self.self.all_snake[0].move()
+
+
